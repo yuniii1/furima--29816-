7da05edd022adae1dd1e9800e20b3d8b006139b9
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-15 17:22:50 +0900

    バリデーションの変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, messages: 'is invalid. Input full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, messages: 'is invalid. Input full-width characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, messages: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, messages: 'is invalid. Input full-width katakana characters.' }
+    VALD_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    validates :first_name, format: { with: VALD_NAME_REGEX , messages: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: VALD_NAME_REGEX , messages: 'is invalid. Input full-width characters.' }
+    VALID_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :first_name_kana, format: { with: VALID_NAME_REGEX , messages: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_kana, format: { with: VALID_NAME_REGEX , messages: 'is invalid. Input full-width katakana characters.' }
     validates :date_select
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, messages: 'is invalid. Input half-width characters.'}
   end
-  # validates :nickname, presence: true, length: { maximum: 6 }, :email, uniqueness: true
 end
