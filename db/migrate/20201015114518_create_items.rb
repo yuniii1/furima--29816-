d6ca81dd13496c4c72e3bcd41c1bb54f79fcde3c
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-18 14:31:15 +0900

    アクティブハッシュ作成

---
+++ db/migrate/20201015114518_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.integer :category_id
+      t.integer :condition_id
+      t.integer :delivery_date_id
+      t.integer :shipping_id
+      t.integer :shipment_source_id
+      t.references :user
+      t.integer :price
+      t.timestamps
+    end
+  end
+end
