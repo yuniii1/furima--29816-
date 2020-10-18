74966d9f1bb631461ab893e199f60237e984880c
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-18 14:33:24 +0900

    カテゴリーとバリデーションの作成

---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :shipment_source
+  
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :condition_id
+    validates :delivery_date_id
+    validates :shipping_id
+    validates :shipment_source_id
+    validates :category_id
+  end
+  validates :price, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
+end
