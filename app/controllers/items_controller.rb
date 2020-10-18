a696e1f55a03960bbf460fd3201ccf5adc57e34e
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-18 14:33:51 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_date_id, :shipping_id, :shipment_source_id, :user, :price, :image,).merge(user_id: current_user.id)
+  end
+
 end
+
