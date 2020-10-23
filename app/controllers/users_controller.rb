5380926998b4583e7ad602c156951c6285bfb029
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-23 15:24:53 +0900

    ヘロクシンタックスエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def update
     if current_user.update(user_params)
       redirect_to root_path
-    else
+      else
       render :edit
+    end
   end
 
   private
