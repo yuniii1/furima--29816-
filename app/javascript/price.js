4f38704e67363e9eb3ca20c4f8f8e6a2db151057
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-23 12:06:54 +0900

    コンソール削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,11 +5,8 @@
   
     list.addEventListener("keyup", () => {
       let amount = list.value;
-      console.log(amount)
       let add_tax_price = Math.ceil(amount * 0.1);
-      console.log(add_tax_price)
       let profit_number = Math.floor(amount * 0.9);
-      console.log(profit_number)
 
       tax.innerHTML = add_tax_price
       profit.innerHTML = profit_number
