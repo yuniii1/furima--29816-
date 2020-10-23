604c5bbb05309ef7c743343bd9a9babaa93c26eb
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-10-23 11:13:39 +0900

    ファクトトリー

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name                         { '商品名を記入してください' }
+    description                  { 'a' }
+    price                        { 500 }
+    condition_id                 { 2 }
+    delivery_date_id             { 2 }
+    shipping_id                  { 2 }
+    shipment_source_id           { 2 }
+    category_id                  { 2 }
+
+    # trait :image do
+    #   image { File.new("#{Rails.root}/spec/images/test_image.png") }
+    # end
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
