acd704d03d941b96ac77ac4c5f0594cc53ed9b10
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-11-04 21:52:04 +0900

    if文のvalidを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    .save
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
   
+  private
+  def order_params
+    params.permit(:item_id, :postal_code, :prefectures_id, :municipality, :address, :building_name, :tell_phone, :purchase).merge(user_id: current_user.id)
+  end
 
 end
