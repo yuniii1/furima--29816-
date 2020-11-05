bdddfcaacd1d2c17821b1718858f24f4fdede787
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-11-05 12:28:48 +0900

    実装条件上から３つ実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:create]
   
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
   def create
@@ -16,7 +21,6 @@ class OrdersController < ApplicationController
   
   private
   def order_params
-    params.permit(:item_id, :postal_code, :prefectures_id, :municipality, :address, :building_name, :tell_phone, :purchase).merge(user_id: current_user.id)
+    params.repuire(:order_address).permit(:postal_code, :prefectures_id, :municipality, :address, :building_name, :tell_phone, :purchase).merge(item_id: params[:item_id],user_id: current_user.id)
   end
-
 end
