411d61a3ad3dcfae309f82c3d4d76a1e364f4a58
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-11-06 12:24:18 +0900

    画像、価格、商品説明分が表示される

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: [:create]
   before_action :set_item, only: [:index, :create]
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
     if current_user.id == @item.user_id || @item.purchase.present?
       redirect_to root_path
