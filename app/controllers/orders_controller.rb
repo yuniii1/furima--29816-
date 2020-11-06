29d4c17520a0da37c4d4006c08ed5b833cda456a
yuniii1 <71069110+yuniii1@users.noreply.github.com>
2020-11-06 11:45:06 +0900

    orders_comtroller内をbefore_actionでまとめた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: [:create]
-  
+  before_action :set_item, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id || @item.purchase.present?
       redirect_to root_path
     end
@@ -11,7 +10,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -33,6 +31,10 @@ class OrdersController < ApplicationController
         card: order_params[:token],
         currency: 'jpy'
       )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
